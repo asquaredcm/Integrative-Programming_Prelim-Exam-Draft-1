efa4d1ed7dac5f1fb3ce95195b736102da8d1b4f
23-0311C <23-0311C@CCL2-32>
2025-03-12 12:35:43 +0800

    Add project files.

---
+++ Integrative Programming_Prelim Exam Draft 1/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Integrative_Programming_Prelim_Exam_Draft_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
